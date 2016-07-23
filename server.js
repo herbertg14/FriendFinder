0658cee4d59ba1b135f2167cb8b4665f8dac878d
herbertg14 <herbert.gutierrez@utexas.edu>
2016-07-23 17:54:49 -0500

    adding alerts

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var path = require("path");
 
 
 var app = express();
-var PORT = process.env.PORT || 80;
+var PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
