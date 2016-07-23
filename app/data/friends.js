2a002340f658e240a4500cfe9816ac649e0e8a50
herbertg14 <herbert.gutierrez@utexas.edu>
2016-07-23 17:06:22 -0500

    finished with local host only

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,6 +4,11 @@ var friendsArray = [
 		name:"Ahmed",
 		photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
 		scores:[5,1,4,4,5,1,2,5,4,1]
+	},
+	{
+		name:"Herbert",
+		photo:"https://avatars2.githubusercontent.com/u/8680797?v=3&s=460",
+		scores:[4,5,4,3,4,1,5,2,2,3]
 	}];
 
 module.exports= friendsArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -10,6 +10,6 @@ module.exports = function(app){
 
 	app.post("/api/friends", function(req,res){
 		friendData.push(req.body);
-		res.json(true);
+		res.json(friendData);
 	});
 }
